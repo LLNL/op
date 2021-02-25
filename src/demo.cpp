9fb535894982e3ae9582c2dfce9fc49186ffaf02
Jonathan Wong <>
2021-02-25 11:50:04 -0800

    catch poorly working case

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,11 +1,16 @@
 #include "op.hpp"
+#include <iostream>
 
 int main(int argc, char*argv[])
 {
   if (argc > 1) {
     auto optimizer = op::PluginOptimizer<op::Optimizer>(argv[1]);
-    optimizer->Go();
-    optimizer->UpdatedVariableCallback();
+    if (optimizer) {
+      optimizer->Go();
+      optimizer->UpdatedVariableCallback();
+    } else {
+      std::cout << "The optimizer did not load properly:" << argv[1] << std::endl;
+    }
   }
   return 0;
 }
