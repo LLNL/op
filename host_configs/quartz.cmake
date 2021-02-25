9146915cc4e664898df849f1d9f501099f3f4300
Jonathan Wong <>
2021-02-24 16:13:28 -0800

    added host_config file, but you still need to supply BLT_SOURCE_DIR on the configureline atm

---
+++ host_configs/quartz.cmake
@@ -0,0 +1,4 @@
+set(ENABLE_MPI ON CACHE BOOL "")
+set(USE_NLOPT ON CACHE BOOL "")
+set(USE_LIDO OFF CACHE BOOL "")
+set(NLOPT_DIR "/usr/gapps/transport-opt/transport-tpls/uberenv_libs/gcc-8.3.1/nlopt-2.6.1/" CACHE PATH "")
