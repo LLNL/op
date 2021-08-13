78e4c3de0069d346b3281ab0a5a257d2b6768da0
Jonathan Wong <>
2021-08-13 16:24:37 -0700

    get rid of unused cmake defines

--- host_configs/quartz.cmake
+++ host_configs/quartz.cmake
@@ -1,4 +1,2 @@
 set(ENABLE_MPI ON CACHE BOOL "")
 set(USE_NLOPT ON CACHE BOOL "")
-set(USE_LIDO OFF CACHE BOOL "")
-set(NLOPT_DIR "/usr/gapps/transport-opt/transport-tpls/uberenv_libs/gcc-8.3.1/nlopt-2.6.1/" CACHE PATH "")
